99864b085797a5eb136c441c29053416e9f01a2b
Saki Ebina <ebiebi0u0@gmail.com>
2024-07-09 16:59:19 -0700

    finished ex1

--- js/script.js
+++ js/script.js
@@ -1,1 +1,21 @@
 // YOUR CODE HERE
+
+const btn1 = document.getElementById("btn1")
+const btn2 = document.getElementById("btn2")
+const btn3 = document.getElementById("btn3")
+const btn4 = document.getElementById("btn4")
+const btn5 = document.getElementById("btn5")
+const paragraphs = document.querySelectorAll('p')
+
+btn1.addEventListener('click', () =>  {
+  paragraphs.forEach((paragraph) => {
+    paragraph.classList.add('pColor')
+  });
+});
+
+btn2.addEventListener('click', () =>  {
+  const secondP =document.createElement('p')
+  secondP.textContent = "Hello World"
+});
+
+
